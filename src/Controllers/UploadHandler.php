226f06493e5abfb471348168469ca08e783d8ccb
acr <acarbey15@gmail.com>
2017-12-27 21:42:55 +0300

    Yaz

--- src/Controllers/UploadHandler.php
+++ src/Controllers/UploadHandler.php
@@ -1120,91 +1120,96 @@ class UploadHandler extends Controller
         $file->name     = $this->get_file_name($uploaded_file, $name, $size, $type, $error,
             $index, $content_range);
 
-        $file_dot      = strtolower(pathinfo($file->name, PATHINFO_EXTENSION));
-        $org_name      = str_replace('.' . $file_dot, '', $name);
-        $acr_file_name = self::ingilizceYap($org_name);
-        if (file_exists(base_path() . '/public_html/acr_files/' . $this->options['acr_file_id'] . '/' . $acr_file_name . '.' . $file_dot)) {
-            $acr_file_name = $acr_file_name . '-' . uniqid(rand(10000, 99999));
-        } else {
-            $acr_file_name = $acr_file_name;
-        }
-        $file->name = $acr_file_name . '.' . $file_dot;
-        $file->size = $this->fix_integer_overflow((int)$size);
-        $file->type = $type;
-        if ($this->validate($uploaded_file, $file, $error, $index)) {
-            $this->handle_form_data($file, $index);
-            $upload_dir = $this->get_upload_path();
-            if (!is_dir($upload_dir)) {
-                mkdir($upload_dir, $this->options['mkdir_mode'], true);
+        $file_dot = strtolower(pathinfo($file->name, PATHINFO_EXTENSION));
+        if (in_array($file_dot, ['jpg', 'gif', 'png', 'bmp', 'tif', 'jpeg', 'rar', 'zip', '7z', 'mp4', 'm4a', 'mp3', 'mov', 'vma'])) {
+
+
+            $org_name      = str_replace('.' . $file_dot, '', $name);
+            $acr_file_name = self::ingilizceYap($org_name);
+            if (file_exists(base_path() . '/public_html/acr_files/' . $this->options['acr_file_id'] . '/' . $acr_file_name . '.' . $file_dot)) {
+                $acr_file_name = $acr_file_name . '-' . uniqid(rand(10000, 99999));
+            } else {
+                $acr_file_name = $acr_file_name;
             }
-            $file_path   = $this->get_upload_path($file->name);
-            $append_file = $content_range && is_file($file_path) &&
-                $file->size > $this->get_file_size($file_path);
-            if ($uploaded_file && is_uploaded_file($uploaded_file)) {
-                // multipart/formdata uploads (POST method uploads)
-                if ($append_file) {
+            $file->name = $acr_file_name . '.' . $file_dot;
+            $file->size = $this->fix_integer_overflow((int)$size);
+            $file->type = $type;
+            if ($this->validate($uploaded_file, $file, $error, $index)) {
+                $this->handle_form_data($file, $index);
+                $upload_dir = $this->get_upload_path();
+                if (!is_dir($upload_dir)) {
+                    mkdir($upload_dir, $this->options['mkdir_mode'], true);
+                }
+                $file_path   = $this->get_upload_path($file->name);
+                $append_file = $content_range && is_file($file_path) &&
+                    $file->size > $this->get_file_size($file_path);
+                if ($uploaded_file && is_uploaded_file($uploaded_file)) {
+                    // multipart/formdata uploads (POST method uploads)
+                    if ($append_file) {
+                        file_put_contents(
+                            $file_path,
+                            fopen($uploaded_file, 'r'),
+                            FILE_APPEND
+                        );
+                    } else {
+
+                        $data = [
+                            'acr_file_id' => $this->options['acr_file_id'],
+                            'file_name_org' => $org_name,
+                            'file_name' => $acr_file_name,
+                            'file_size' => $file->size,
+                            'file_type' => $file_dot,
+                        ];
+                        $acr_file_model->child_fields_create($data);
+                        move_uploaded_file($uploaded_file, $file_path);
+                        if ($this->options['e_okul'] == 1) {
+                            $img = Image::make($file_path);
+                            $img->fit(230);
+                            $img->crop(133, 177, 50, 0);
+                            if (!is_dir(base_path() . '/public_html/acr_files/' . $this->options['acr_file_id'] . '/e_okul/')) {
+                                mkdir(base_path() . '/public_html/acr_files/' . $this->options['acr_file_id'] . '/e_okul/');
+                            }
+                            if (!is_dir(base_path() . '/public_html/acr_files/' . $this->options['acr_file_id'] . '/temp/')) {
+                                mkdir(base_path() . '/public_html/acr_files/' . $this->options['acr_file_id'] . '/temp/');
+                            }
+                            $img->limitColors(255, '#ff998d');
+                            $img->limitColors(255, '#ff998d');
+
+                            $img->save(base_path() . '/public_html/acr_files/' . $this->options['acr_file_id'] . '/temp/' . $acr_file_name . '.jpg', 100);
+                            $img_2 = Image::make(base_path() . '/public_html/acr_files/' . $this->options['acr_file_id'] . '/temp/' . $acr_file_name . '.jpg');
+                            $img_2->limitColors(255, '#ff998d');
+                            $img_2->limitColors(255, '#ff998d');
+                            $img_2->save(base_path() . '/public_html/acr_files/' . $this->options['acr_file_id'] . '/e_okul/' . $acr_file_name . '.jpg', 100);
+
+
+                        }
+                    }
+                } else {
+                    // Non-multipart uploads (PUT method support)
                     file_put_contents(
                         $file_path,
-                        fopen($uploaded_file, 'r'),
-                        FILE_APPEND
+                        fopen($this->options['input_stream'], 'r'),
+                        $append_file ? FILE_APPEND : 0
                     );
+                }
+                $file_size = $this->get_file_size($file_path, $append_file);
+                if ($file_size === $file->size) {
+                    $file->url = $this->get_download_url($file->name);
+                    if ($this->is_valid_image_file($file_path)) {
+                        $this->handle_image_file($file_path, $file);
+                    }
                 } else {
-
-                    $data = [
-                        'acr_file_id' => $this->options['acr_file_id'],
-                        'file_name_org' => $org_name,
-                        'file_name' => $acr_file_name,
-                        'file_size' => $file->size,
-                        'file_type' => $file_dot,
-                    ];
-                    $acr_file_model->child_fields_create($data);
-                    move_uploaded_file($uploaded_file, $file_path);
-                    if ($this->options['e_okul'] == 1) {
-                        $img = Image::make($file_path);
-                        $img->fit(230);
-                        $img->crop(133, 177, 50, 0);
-                        if (!is_dir(base_path() . '/public_html/acr_files/' . $this->options['acr_file_id'] . '/e_okul/')) {
-                            mkdir(base_path() . '/public_html/acr_files/' . $this->options['acr_file_id'] . '/e_okul/');
-                        }
-                        if (!is_dir(base_path() . '/public_html/acr_files/' . $this->options['acr_file_id'] . '/temp/')) {
-                            mkdir(base_path() . '/public_html/acr_files/' . $this->options['acr_file_id'] . '/temp/');
-                        }
-                        $img->limitColors(255, '#ff998d');
-                        $img->limitColors(255, '#ff998d');
-
-                        $img->save(base_path() . '/public_html/acr_files/' . $this->options['acr_file_id'] . '/temp/' . $acr_file_name . '.jpg', 100);
-                        $img_2 = Image::make(base_path() . '/public_html/acr_files/' . $this->options['acr_file_id'] . '/temp/' . $acr_file_name . '.jpg');
-                        $img_2->limitColors(255, '#ff998d');
-                        $img_2->limitColors(255, '#ff998d');
-                        $img_2->save(base_path() . '/public_html/acr_files/' . $this->options['acr_file_id'] . '/e_okul/' . $acr_file_name . '.jpg', 100);
-
-
+                    $file->size = $file_size;
+                    if (!$content_range && $this->options['discard_aborted_uploads']) {
+                        unlink($file_path);
+                        $file->error = $this->get_error_message('abort');
                     }
                 }
-            } else {
-                // Non-multipart uploads (PUT method support)
-                file_put_contents(
-                    $file_path,
-                    fopen($this->options['input_stream'], 'r'),
-                    $append_file ? FILE_APPEND : 0
-                );
+                $this->set_additional_file_properties($file);
             }
-            $file_size = $this->get_file_size($file_path, $append_file);
-            if ($file_size === $file->size) {
-                $file->url = $this->get_download_url($file->name);
-                if ($this->is_valid_image_file($file_path)) {
-                    $this->handle_image_file($file_path, $file);
-                }
-            } else {
-                $file->size = $file_size;
-                if (!$content_range && $this->options['discard_aborted_uploads']) {
-                    unlink($file_path);
-                    $file->error = $this->get_error_message('abort');
-                }
-            }
-            $this->set_additional_file_properties($file);
+
+            return $file;
         }
-        return $file;
     }
 
     protected function readfile($file_path)
